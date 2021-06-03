e7be261620de0c6f9a2ae57571544adccc88edb8
wallydrag <debashish.ghatak@gmail.com>
2021-06-03 18:22:07 +0530

    attempt of api

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
+var axios = require("axios");
+var FormData = require("form-data");
 
-try {
-  const odetFile = core.getInput('odet-file');
-  const apiKey = core.getInput('api-key');
-  console.log(`${odetFile}`);
-  console.log(`${apiKey}`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-} catch (error) {
-  core.setFailed(error.message);
-}
+var data = new FormData();
+const odetFile = core.getInput("odet-file");
+const apiKey = core.getInput("api-key");
+data.append("yaml", odetFile);
+var config = {
+  method: "post",
+  url: "https://odet-staging.herokuapp.com/services",
+  headers: {
+    "X-ODET-KEY": apiKey,
+  },
+  data: data,
+};
+axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+const time = new Date().toTimeString();
+core.setOutput("time", time);
